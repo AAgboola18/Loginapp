d02c44bb2064068f81f1323311fb5e60fcd6d341
A_Agboola18 <A_Agboola18@cranford.hounslow.sch.uk>
2024-04-30 15:05:02 +0100

    trying to implement a registration page

--- Loginapp.py
+++ Loginapp.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title("Login")
 Username = st.text_input("Username")
-password = st.textP_input("Password", type="password")
+password = st.text_input("Password", type="password")
 
 
 st.button("Login")
