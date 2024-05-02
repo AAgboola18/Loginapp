f1260d7cd774d9e882da54455bea98a0e3e6c25e
A_Agboola18 <A_Agboola18@cranford.hounslow.sch.uk>
2024-05-02 11:28:38 +0100

    Made a Registration page and made it take a wrong password

--- Loginapp.py
+++ Loginapp.py
@@ -6,4 +6,11 @@ Username = st.text_input("Username")
 password = st.text_input("Password", type="password")
 
 
-st.button("Login")
+if st.button("Login"):
+    if Username == "admin" and password == "admin1":
+        st.success("Logging successful ")
+    else:
+        st.error("Invalid username or password.")
+
+
+
--- pages/Registration.py
+++ pages/Registration.py
@@ -1,5 +1,15 @@
 import streamlit as st
 
 st.title("Registration page")
+
 Firstname = st.text_input("First Name")
 Surname = st.text_input("Surname")
+Password = st.text_input("Password", type="password")
+confirm_pass = st.text_input("Confirm Password", type="password")
+
+if st.button("Register"):
+    if Password == confirm_pass:
+        st.success("Registration successful!")
+    else:
+        st.error("Password do not match")
+
